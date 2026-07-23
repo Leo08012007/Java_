08ba732c8a27597072338f15086d286cb7c8de7e
mokshitha <mokshithatailuru@gmail.com>
2026-07-23 23:34:42 +0530

    advanced pattern try

---
+++ java_practiece/day3.java
@@ -0,0 +1,25 @@
+/*class day3 {
+    public static void main(String args[]){
+        int n=6;
+        for (int row=1;row<n;row++){
+            for (int col =1;col<=row;col++){
+                System.out.print(col+" ");
+
+            }
+            System.out.println();       
+        }
+
+    }}
+/* */
+    class day3 {
+    public static void main(String args[]){
+        int n=6;
+        for (int row=1;row<n;row++){
+            for (int col =1;col<=n;col--){
+                System.out.print(col+" ");
+
+            }
+            System.out.println();       
+        }
+
+    }}
---
+++ java_practiece/day4.java
@@ -0,0 +1,25 @@
+public class day4 {
+    public static void main(String args[]){
+        int n=8;
+        for (int i=1;i<=n;i++){
+            for(int j=1;j<=i;j++){
+int stars=2*(n-i);
+
+            }
+            for(int k=6;k>=0;k+=2){
+                System.out.print("  ");
+            }
+            for(int l=1;l<=n;l++){
+                System.out.print("*");
+            }
+           /*  System.out.print(" ");
+        }
+        for(int i=0;i<n;i++){
+            for (int j=2;j<=n;j+=2){
+                System.out.print("*");
+            }*/
+            System.out.println();
+        }
+    }
+    
+}
