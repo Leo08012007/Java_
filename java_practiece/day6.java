74c6c124de6588578eeaba8209c8c2ea95e4cc23
mokshitha <mokshithatailuru@gmail.com>
2026-07-28 16:48:03 +0530

    day 6 - functions

---
+++ java_practiece/day6.java
@@ -0,0 +1,58 @@
+/*import java.util.*;
+public class day6 {
+    public static int avg(int a, int b, int c){
+        int res=(a+b+c)/3;
+        System.out.println(res);
+        return res;
+
+    }
+    public static void main(String args[]){
+        Scanner sc = new Scanner (System.in);
+        int a=sc.nextInt();
+        int b=sc.nextInt();
+        int c=sc.nextInt();
+        avg(a,b,c);
+
+        }
+    }
+    
+
+import java.util.*;
+class day6{
+    public static void OddNum(int n){
+        for(int i=0;i<=n;i++){
+            if(i%2!=0){
+                System.out.print(i+" ");
+            }
+        }
+        return ;
+    }
+    public static void main(String args[]){
+        Scanner sc=new Scanner (System.in);
+            int num=sc.nextInt();
+            OddNum(num);
+        }
+    
+}
+       
+import java.util.*;
+class day6{
+    public static void GratestOfTwo(int a,int b){
+        if(a>b){
+            System.out.println(a);
+            
+        }else{
+            System.out.println(b);
+        }
+
+    }
+    public static void main(String args[]){
+        Scanner sc=new Scanner (System.in);
+            int n1=sc.nextInt();
+            int n2=sc.nextInt();
+            GratestOfTwo(n1,n2);
+        }
+    
+}
+ */
+
