cba6bcd32400857ca799cc74ba4d1eda88b3fab4
mokshitha <mokshithatailuru@gmail.com>
2026-07-31 17:49:49 +0530

    max and min of array

--- java_practiece/day8.java
+++ java_practiece/day8.java
@@ -1,4 +1,4 @@
-import java.util.Scanner;
+/*import java.util.Scanner;
 class day8{
     public static void main(String args[]){
         Scanner sc=new Scanner (System.in);
@@ -20,3 +20,54 @@ class day8{
     }
 
 }
+import java.util.Scanner;
+class day8{
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        String arr[]=new String [n];
+        for(int i=0;i<n;i++){
+            arr[i]=sc.nextLine();
+            
+        }
+        for(int i=0;i<n;i++){
+        System.out.println(arr[i]);
+        }
+            }
+        } 
+
+    
+
+*/
+import java.util.Scanner;
+class day8{
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int arr[]=new int [n];
+        for(int i=0;i<n;i++){
+            arr[i]=sc.nextInt();
+            
+        }
+        int max=arr[0];
+        int min=arr[0];
+        for(int i=1;i<n;i++){
+            if(max<arr[i]){
+                max=arr[i];
+            }   
+            if(min>arr[i]){
+                min=arr[i];
+            }
+        }
+    
+            System.out.println("The minimum value of the array is :"+ min);
+            System.out.println("The maximum value of the arrray is : "+max);
+    }
+        
+
+            }
+        
+
+    
+
+
