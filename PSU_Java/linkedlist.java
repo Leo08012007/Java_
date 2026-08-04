c860de3c9111ce10187951aa894189ce6e67ae26
mokshitha <mokshithatailuru@gmail.com>
2026-08-04 10:20:24 +0530

    reversing of array

---
+++ PSU_Java/linkedlist.java
@@ -0,0 +1,9 @@
+public class linkedlist {
+    boolean isPlaindrome(ListNode head){
+        ListNode slow=head;
+        ListNode fast=head;
+        while(fast!=null && fast.next!=null){
+            
+        }
+    }
+}
--- java_practiece/day10.java
+++ java_practiece/day10.java
@@ -1,4 +1,4 @@
-import java.util.Scanner;
+/*import java.util.Scanner;
 public class day10 {
     public static void main(String args[]){
         Scanner sc= new Scanner (System.in);
@@ -21,3 +21,52 @@ public class day10 {
     }
     
 }
+import java.util.Scanner;
+public class day10 {
+    public static void main(String args[]){
+        Scanner sc= new Scanner (System.in);
+        int rows=sc.nextInt();
+        int cols=sc.nextInt();
+        int [][] arr=new int[rows][cols];
+        for(int i=0;i<rows;i++){
+            for(int j=0;j<cols;j++){
+                arr[i][j]=sc.nextInt();
+            }
+        }
+         int max=0;
+        for(int i=0;i<rows;i++){
+            int csum=0;
+            for(int j=0;j<cols;j++){
+                csum=csum+ arr[i][j];
+                
+            }
+            System.out.println(csum);
+            if(max<csum){
+                max=csum;
+            }
+            
+
+        }
+        System.out.println(max);
+    }
+}
+*/
+// reversing of an array
+
+import java.util.Scanner;
+public class day10 {
+    public static void main(String args[]){
+        Scanner sc= new Scanner (System.in);
+        int n=sc.nextInt();
+        int []arr=new int[n];
+        for(int i=0;i<n;i++){
+                arr[i]=sc.nextInt();
+            }
+            int res[]=new int[n];
+            for(int i=0;i<n;i++ ){
+               res[i]=arr[n-(i+1)];
+               System.out.println(res[i]);
+            }
+
+        }
+    }
