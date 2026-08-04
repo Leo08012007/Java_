eed35187d4b09cda123f70476549d30fddb9b5b4
mokshitha <mokshithatailuru@gmail.com>
2026-08-04 23:35:33 +0530

    reversing of array

--- PSU_Java/linkedlist.java
+++ PSU_Java/linkedlist.java
@@ -1,9 +1,11 @@
+import java.util.*;
 public class linkedlist {
     boolean isPlaindrome(ListNode head){
         ListNode slow=head;
         ListNode fast=head;
         while(fast!=null && fast.next!=null){
-            
+            slow=slow.next;
         }
     }
 }
+
--- java_practiece/day10.java
+++ java_practiece/day10.java
@@ -50,7 +50,7 @@ public class day10 {
         System.out.println(max);
     }
 }
-*/
+
 // reversing of an array
 
 import java.util.Scanner;
@@ -62,11 +62,76 @@ public class day10 {
         for(int i=0;i<n;i++){
                 arr[i]=sc.nextInt();
             }
-            int res[]=new int[n];
+            /*int res[]=new int[n];
             for(int i=0;i<n;i++ ){
                res[i]=arr[n-(i+1)];
                System.out.println(res[i]);
             }
+        int arr2[]=new int[n];
+        for(int i=0;i<n;i++){
+            arr2[i]=arr[i];
+            System.out.print(arr2[i]+" ");
+        }
 
+        }
+    }
+
+import java.util.Scanner;
+public class day10 {
+    public static void main(String args[]){
+        Scanner sc= new Scanner (System.in);
+        int n=sc.nextInt();
+        int []arr=new int[n];
+        int firstc=0;
+        for(int i=0;i<n;i++){
+                arr[i]=sc.nextInt();
+            }
+            int count=0;
+        for(int i=0;i<n;i++){
+            if(arr[i]==1){
+                count++;
+                firstc=count;
+            }
+            if(arr[i]==0){
+                count=0;
+            }
+        }
+        System.out.println(firstc);
+        System.out.println(count);
+        }
+        }
+    */
+   import java.util.*;
+public class day10 {
+    public static void main(String args[]){
+        Scanner sc= new Scanner (System.in);
+        int n=sc.nextInt();
+        int []arr=new int[n];
+        int firstc=0;
+        for(int i=0;i<n;i++){
+                arr[i]=sc.nextInt();
+            }
+        int max=arr[0];
+            int min=arr[0];
+        for(int i=1;i<n;i++){
+            if(max<arr[i]){
+                max=arr[i];
+            }
+            if(min>arr[0]){
+                min=arr[i];
+            }
+        }
+            int MaxMin[]=new int[2];
+            MaxMin[0]=max;
+            MaxMin[1]=min;
+        System.out.println(Arrays.toString(MaxMin));
+        for(int i=min;i<max;i++){
+            int values=i;
+            System.out.println(values);
+            for(int j=0;j<arr.length;j++){
+                
+            }
+
+        }
         }
     }
